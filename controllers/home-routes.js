e22d95c918b9eec03cd8913f08880dc02cdeb36e
Joshua Sands <josh.sands@mail.com>
2021-08-05 16:58:00 -0500

    added user login and logout

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
+const { escapeExpression } = require('handlebars');
 const sequelize = require('../config/connection');
 const { Post, User, Comment, Vote } = require('../models');
 
 // get all posts for homepage
 router.get('/', (req, res) => {
-  console.log('======================');
+  console.log('you are now logged in to your session' + req.session);
   Post.findAll({
     attributes: [
       'id',
@@ -28,15 +29,27 @@ router.get('/', (req, res) => {
       }
     ]
   })
-    .then(dbPostData => {
-      const posts = dbPostData.map(post => post.get({ plain: true }));
-
-      res.render('homepage', { posts });
-    })
+  .then(dbUserData => {
+    req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+  
+      res.json(dbUserData);
+    });
+  })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect ('/');
+    return;
+  }
+  res.render('login');
 });
 
 module.exports = router;
